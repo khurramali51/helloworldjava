2785ebe9ca93204a73dde342baeaad24ea7a398a
Khurram <ali.kakhan@gmail.com>
2022-08-15 22:41:52 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World version 2.0 @10:26 - nishant cinema");
+    System.out.println("Hello World version 2.0 @10:26 - why error is coming");
     }
 }
