1c383b0dd75dd10b0a0d29736175ef74e491c1e4
Khurram <ali.kakhan@gmail.com>
2022-08-15 22:44:17 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World version 2.0 @10:26 - why error is coming");
+    System.out.println("Hello World version 2.0 @10:26 - ");
+    System.out.println("for webhooks url : the protocol should be http:// not https://")
     }
 }
