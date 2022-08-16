a23d0efdf88ba46d1ef8bdec2dfcf74be3f00d84
Khurram <ali.kakhan@gmail.com>
2022-08-15 22:40:30 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World version 2.0 @10:26 - second trigger by webhook");
+    System.out.println("Hello World version 2.0 @10:26 - nishant cinema");
     }
 }
