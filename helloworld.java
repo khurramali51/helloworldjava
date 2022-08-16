8c2500149cc2572a41e865298a5df8a728535ad7
Khurram <ali.kakhan@gmail.com>
2022-08-15 22:26:42 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World version 1.0");
+    System.out.println("Hello World version 2.0 @10:26");
     }
 }
